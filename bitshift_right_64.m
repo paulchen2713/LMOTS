6d3dde34bcb3c297f9b5cea8d45f235e6098f3b7
Shao-Heng <95068443+paulchen2713@users.noreply.github.com>
2022-05-18 19:56:31 +0800

    Add files via upload

---
+++ bitshift_right_64.m
@@ -0,0 +1,35 @@
+%
+% bitshift_right_64
+%
+function out=bitshift_right_64(a_hex,num)
+a=a_hex;
+shift_num=num;
+a_left=a(1:8);
+a_right=a(9:16);
+a_left_dec=uint32(hex2dec(a_left));
+a_right_dec=uint32(hex2dec(a_right));
+if shift_num < 32
+    temp_left=bitget(a_left_dec,1:shift_num);
+    a_left_dec=bitshift(a_left_dec,-shift_num);
+    a_right_dec=bitshift(a_right_dec,-shift_num);
+    for i=1:shift_num
+        a_right_dec=bitset(a_right_dec,32-shift_num+i,temp_left(i));
+    end
+elseif shift_num == 32
+    a_right_dec=a_left_dec;
+    a_left_dec=0;
+else
+    temp_left=bitget(a_left_dec,shift_num-32+1:32);
+    a_left_dec=uint32(0);
+    a_right_dec=uint32(0);
+    for i=1:64-shift_num
+        a_right_dec=bitset(a_right_dec,i,temp_left(i)) ;
+    end
+end
+a_left_hex=dec2hex(a_left_dec,8);
+a_right_hex=dec2hex(a_right_dec,8);
+a_hex=strcat(a_left_hex,a_right_hex);
+out=a_hex;
+return
+
+
