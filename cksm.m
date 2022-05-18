97cf8a15ce5c0943a71645c7f48e394d3301730b
Shao-Heng <95068443+paulchen2713@users.noreply.github.com>
2022-05-18 20:02:38 +0800

    Update cksm.m

--- cksm.m
+++ cksm.m
@@ -2,13 +2,13 @@
 % Checksum function
 %
 function out = cksm(S, n, w, ls)
-sum = 0;
-nw = n*8/w;
-w2 = 2^w - 1;
-for i = 1 : nw
-    sum = sum + w2 - coef(S, i-1, w);
-end
-sum = bitshift(sum, ls); % shift left ls bits
-out = dec2hex(sum, 4);
-
+    sum = 0;
+    nw = n*8 / w;
+    w2 = 2^w - 1;
+    for i = 1:nw
+        sum = sum + w2 - coef(S, i - 1, w);
+    end
+    sum = bitshift(sum, ls); % shift left ls bits
+    out = dec2hex(sum, 4);
 return
+
